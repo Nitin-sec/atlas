827af951a46821a5155901a4233588404e4a761d
Nitin Dahiya <nitindahiya131@gmail.com>
2026-07-18 22:19:14 +0530

    docs: document database engine responsibility

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,9 @@
-from sqlalchemy import create_engine
+"""
+Centralized database engine for Atlas
+All database interaction should use this shared engine
+"""
 
+from sqlalchemy import create_engine
 from app.core.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
-
